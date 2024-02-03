49d8bae0aed21e68f9697d298cd54e55ed6d411d
flormedinav <flormedinav7@gmail.com>
2024-02-03 13:19:38 -0300

    middleware files

---
+++ server/middlewares/authorizationMiddleware.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+
+const authorizationMiddleware = (req, res, next) => {
+  const authorizationHeader = req.headers.authorization;
+
+  if (!authorizationHeader || !authorizationHeader.startsWith("Bearer ")) {
+    return res.status(401).json({
+      message: "No token, authorization denied",
+    });
+  }
+
+  const token = authorizationHeader.substring(7);
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      console.log({ err });
+      if (err.name === "TokenExpiredError") {
+        return res.status(401).json({ message: "Token expired" });
+      } else {
+        return res.status(403).json({ message: "Invalid token" });
+      }
+    }
+    
+    if (req.params.userId !== user.id) {
+      return res.status(403).json({ message: "Unauthorized access" });
+    }
+
+    req.user = user;
+    next();
+  });
+};
+
+export default authorizationMiddleware;
