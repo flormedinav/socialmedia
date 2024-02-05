9051126390e3112dd216cf7f2e4df1d23a7fabc0
flormedinav <flormedinav7@gmail.com>
2024-02-04 21:45:48 -0300

    add added Logo component

---
+++ client/src/components/Logo/index.jsx
@@ -0,0 +1,44 @@
+import { Typography } from "@mui/material";
+import { useTheme } from "@mui/material";
+
+import { GLOBAL_CONSTANST } from "../../constants/global";
+import { useNavigate } from "react-router-dom";
+
+const Logo = ({ isNavigate }) => {
+  const navigate = useNavigate();
+
+  const { palette } = useTheme();
+  const primaryLight = palette.primary.light;
+
+  const getStylesLogo = () => {
+    if (isNavigate) {
+      return {
+        "&:hover": {
+          color: primaryLight,
+          cursor: isCursorPointer,
+        },
+      };
+    }
+  };
+  const isCursorPointer = isNavigate ? "pointer" : "unset";
+
+  const handleClick = () => {
+    if (!isNavigate) return;
+
+    return () => navigate("/home");
+  };
+
+  return (
+    <Typography
+      fontWeight="bold"
+      fontSize="clamp(1rem, 2rem, 2.25rem)"
+      color="primary"
+      onClick={handleClick}
+      sx={getStylesLogo()}
+    >
+      {GLOBAL_CONSTANST.NAME_SOCIAL_MEDIA}
+    </Typography>
+  );
+};
+
+export default Logo;
