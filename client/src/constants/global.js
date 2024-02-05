04375445965bd918960965106d10d01f743dbd7e
flormedinav <flormedinav7@gmail.com>
2024-02-04 21:42:51 -0300

    constants file

---
+++ client/src/constants/global.js
@@ -0,0 +1,16 @@
+export const TYPE_THEMES = {
+  LIGHT: "light",
+  DARK: "dark",
+};
+
+export const GLOBAL_CONSTANST = {
+  NAME_SOCIAL_MEDIA: "Socialcode",
+  DESCRIPTION_WELCOME: "¡Bienvenidos a Socialcode!",
+};
+
+export const MEDIA_QUERY_MIN_WIDTH = "(min-width: 1000px)";
+
+export const NAVBAR_CONSTANTS = {
+  PLACEHOLDER_SEARCH: "Buscar usuario",
+  LOG_OUT: "Cerrar sesión",
+};
