ca2ebd03c0af40211ef9fe8d284327f131272921
flormedinav <flormedinav7@gmail.com>
2024-02-05 23:07:15 -0300

    add schema files added

---
+++ client/src/schema/loginSchema.js
@@ -0,0 +1,13 @@
+import * as yup from "yup";
+
+import { FORM_CONSTANTS } from "../constants/global";
+
+const loginSchema = yup.object({
+  email: yup
+    .string()
+    .email(FORM_CONSTANTS.ERROR.EMAIL.INVALID_EMAIL)
+    .required(FORM_CONSTANTS.ERROR.EMAIL.REQUIRED),
+  password: yup.string().required(FORM_CONSTANTS.ERROR.PASSWORD.REQUIRED),
+});
+
+export default loginSchema;
---
+++ client/src/schema/registerSchema.js
@@ -0,0 +1,21 @@
+import * as yup from "yup";
+
+import { FORM_CONSTANTS } from "../constants/global";
+
+const registerSchema = yup.object({
+  firstName: yup.string().required(FORM_CONSTANTS.ERROR.FIRST_NAME.REQUIRED),
+  lastName: yup.string().required(FORM_CONSTANTS.ERROR.LAST_NAME.REQUIRED),
+  email: yup
+    .string()
+    .email(FORM_CONSTANTS.ERROR.EMAIL.INVALID_EMAIL)
+    .required(FORM_CONSTANTS.ERROR.EMAIL.REQUIRED),
+  password: yup
+    .string()
+    .min(6, FORM_CONSTANTS.ERROR.PASSWORD.MIN_LENGTH)
+    .required(FORM_CONSTANTS.ERROR.PASSWORD.REQUIRED),
+  location: yup.string().required(FORM_CONSTANTS.ERROR.LOCATION.REQUIRED),
+  ocupation: yup.string().required(FORM_CONSTANTS.ERROR.OCUPATION.REQUIRED),
+  picture: yup.string().required(FORM_CONSTANTS.ERROR.PICTURE.REQUIRED),
+});
+
+export default registerSchema;
