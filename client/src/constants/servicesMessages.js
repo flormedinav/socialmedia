04748aaf8d45263322d63dc5de2a82d5c914288e
flormedinav <flormedinav7@gmail.com>
2024-02-05 23:08:36 -0300

    add services constants added

---
+++ client/src/constants/servicesMessages.js
@@ -0,0 +1,8 @@
+export const SERVICES_MESSAGES = {
+  AUHT: {
+    ERROR: {
+      LOGIN: "Error al iniciar sesión. Por favor, inténtalo de nuevo.",
+      REGISTER: "Error al crear la cuenta. Por favor, inténtalo de nuevo.",
+    },
+  },
+};
