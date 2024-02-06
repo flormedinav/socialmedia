d0bf31fa0eb2f763eeb8c9a69805af103ef955fe
flormedinav <flormedinav7@gmail.com>
2024-02-05 23:09:39 -0300

    md RegisterPage added

---
+++ client/src/pages/index.jsx
@@ -0,0 +1,7 @@
+import HomePage from "./HomePage";
+import NotFoundPage from "./NotFoundPage";
+import LoginPage from "./LoginPage";
+import ProfilePage from "./ProfilePage";
+import RegisterPage from "./RegisterPage";
+
+export { HomePage, NotFoundPage, LoginPage, ProfilePage, RegisterPage };
