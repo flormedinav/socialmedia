59d14accc799d5f1a8fcb048267db434e6137035
flormedinav <flormedinav7@gmail.com>
2024-02-09 21:43:35 -0300

    md new color added

--- client/src/theme.js
+++ client/src/theme.js
@@ -71,6 +71,8 @@ export const themeSettings = (mode) => {
               main: colorTokens.grey[200],
               mediumMain: colorTokens.grey[300],
               medium: colorTokens.grey[400],
+              mediumLight1: colorTokens.grey[500],
+              mediumLight2: colorTokens.grey[600],
               light: colorTokens.grey[700],
             },
             background: {
