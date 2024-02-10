72c3aec2f1cfba09b7106d38069ba31bef3c89c4
flormedinav <flormedinav7@gmail.com>
2024-02-09 21:50:29 -0300

    md fixes general in HomePage

--- client/src/pages/HomePage/index.jsx
+++ client/src/pages/HomePage/index.jsx
@@ -1,7 +1,9 @@
+import { useEffect, useState } from "react";
 import { Box, useMediaQuery } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+
 import Navbar from "../../components/Navbar";
 import { MEDIA_QUERY_MIN_WIDTH } from "../../constants/global";
-import { useDispatch, useSelector } from "react-redux";
 import {
   AdvertWidget,
   FriendListWidget,
@@ -11,12 +13,36 @@ import {
   UserWidget,
 } from "../../components";
 import { setPosts } from "../../state/slices/postsSlice";
-import { useEffect } from "react";
+import { getFeedPosts } from "../../services/postsServices";
+import { setFriends } from "../../state/slices/userSlice";
+import { getUserFriends } from "../../services/usersServices";
 
 const HomePage = () => {
-  const isNonMobileScreens = useMediaQuery(MEDIA_QUERY_MIN_WIDTH[1000]);
-  const { _id } = useSelector((state) => state.user.user);
+  const [loading, setLoading] = useState(false);
+  const { user } = useSelector((state) => state.user);
+  const { posts } = useSelector((state) => state.posts);
+  const { token } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
+  const isNonMobileScreens = useMediaQuery(MEDIA_QUERY_MIN_WIDTH[1000]);
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const [postsData] = await Promise.all([
+        getFeedPosts({ userId: user._id, token }),
+      ]);
+
+      dispatch(setPosts(postsData.data));
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  console.log({ user }, "en home");
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <Box>
@@ -29,21 +55,21 @@ const HomePage = () => {
         justifyContent="space-between"
       >
         <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
-          <UserWidget userId={_id} />
+          <UserWidget userId={user._id} user={user} />
         </Box>
         <Box
           flexBasis={isNonMobileScreens ? "42%" : undefined}
           mt={isNonMobileScreens ? undefined : "2rem"}
         >
           <PostCreator />
-          <PostsWidget userId={_id} />
+          <PostsWidget loading={loading} posts={posts} />
         </Box>
         {isNonMobileScreens && (
           <Box flexBasis="26%">
             {/* <AdvertWidget /> */}
-            <FriendListWidget userId={_id} />
+            <FriendListWidget friends={user.friends} userId={user._id} />
             <Box m="2rem 0" />
-            <SearchFriendWidget userId={_id} />
+            <SearchFriendWidget userId={user._id} />
           </Box>
         )}
       </Box>
