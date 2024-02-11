34ae0e7ed9f4c243fa712b9fb0e4eb72ae01d3f4
flormedinav <flormedinav7@gmail.com>
2024-02-11 19:49:59 -0300

    md add new constants

--- server/constants/globals.js
+++ server/constants/globals.js
@@ -1,6 +1,9 @@
 export const QUERY_SELECT_INFO_USER =
   "firstName lastName picture occupation location posts";
 
+export const QUERY_SELECT_INFO_USER_WITHPOUT_POSTS =
+  "firstName lastName picture occupation location";
+
 export const NOT_SELECT_PASSWORD = "-password";
 
 export const GLOBAL_ERROR_MESSAGES = {
