e361b4ceadc9751fd7659ba6f1ece1d1476c2987
flormedinav <flormedinav7@gmail.com>
2024-02-10 23:16:29 -0300

    add constants not found page added

---
+++ client/src/constants/notFoundConstants.js
@@ -0,0 +1,6 @@
+export const NOT_FOUND_CONSTANTS = {
+  TITLE: "Oops! Página no encontrada",
+  SUBTITLE:
+    "Lo sentimos, la página que estás buscando no se pudo encontrar. Puede que haya sido eliminada o que la URL haya sido ingresada incorrectamente.",
+  TEXT_BUTTON: "Ir a la página principal",
+};
