6780eefd5486cc618bbdae594ad84462f538555e
flormedinav <flormedinav7@gmail.com>
2024-02-12 17:15:33 -0300

    fixes general

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+// const instance = axios.create({
+//   baseURL: `http://localhost:3001/api/v1`,
+// });
+
 const instance = axios.create({
-  baseURL: `http://localhost:3001/api/v1`,
+  baseURL: `https://backend-social-media-ajbq.onrender.com/api/v1`,
 });
 
 export default instance;
