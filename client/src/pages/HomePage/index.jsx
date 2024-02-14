bb0e2a91e6e7bf91b27ddd6f6c9dd03c1eff2ab8
flormedinav <flormedinav7@gmail.com>
2024-02-14 17:52:35 -0300

    md pagination added

--- client/src/pages/HomePage/index.jsx
+++ client/src/pages/HomePage/index.jsx
@@ -1,70 +1,87 @@
 import { useEffect, useState } from "react";
-import { Box, CircularProgress, useMediaQuery } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
-import InfiniteScroll from "react-infinite-scroll-component";
 
 import Navbar from "../../components/Navbar";
 import { MEDIA_QUERY_MIN_WIDTH } from "../../constants/global";
 import {
-  AdvertWidget,
   FriendListWidget,
-  InfiniteScrollBase,
   PostCreator,
   PostsWidget,
   SearchFriendWidget,
   UserWidget,
 } from "../../components";
 
-import useGetFeedPosts from "../../hooks/usePosts/useGetFeedPosts";
+import { getFeedPosts } from "../../services/postsServices";
+import { setPosts } from "../../state/slices/postsSlice";
 
 const HomePage = () => {
-  const { user } = useSelector((state) => state.user);
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.user);
+  const { posts } = useSelector((state) => state.posts);
   const { token } = useSelector((state) => state.auth);
 
   const isNonMobileScreens = useMediaQuery(MEDIA_QUERY_MIN_WIDTH[1000]);
 
-  const { postsData, hasNextPage, fetchNextPage, isLoading } = useGetFeedPosts({
-    userId: user._id,
-    token,
-    userFriends: user.friends,
-  });
+  const fetchPage = async () => {
+    setIsLoading(true);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+
+    const response = await getFeedPosts({
+      userId: user._id,
+      token,
+      page: currentPage,
+    });
+
+    dispatch(setPosts(response.data));
+    setTotalPages(response.pageInfo.totalPages);
+
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchPage();
+  }, [currentPage, user.friends]);
 
   return (
-    <InfiniteScrollBase
-      dataLength={postsData?.dataLength || 0}
-      hasNextPage={hasNextPage}
-      fetchNextPage={fetchNextPage}
+    <Box
+      width="100%"
+      padding="2rem 6%"
+      display={isNonMobileScreens ? "flex" : "block"}
+      gap="0.5rem"
+      justifyContent="space-between"
+      minHeight="100vh"
     >
-      <Navbar isNavigate />
-      <Box
-        width="100%"
-        padding="2rem 6%"
-        display={isNonMobileScreens ? "flex" : "block"}
-        gap="0.5rem"
-        justifyContent="space-between"
-      >
-        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
-          <UserWidget userId={user._id} user={user} />
-        </Box>
-        <Box
-          flexBasis={isNonMobileScreens ? "42%" : undefined}
-          mt={isNonMobileScreens ? undefined : "2rem"}
-        >
-          <PostCreator />
-          <Box m="2rem 0" />
-          <PostsWidget loading={isLoading} posts={postsData} />
-        </Box>
-        {isNonMobileScreens && (
-          <Box flexBasis="26%">
-            {/* <AdvertWidget /> */}
-            <FriendListWidget friends={user.friends} userId={user._id} />
-            <Box m="2rem 0" />
-            <SearchFriendWidget userId={user._id} />
-          </Box>
-        )}
+      <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+        <UserWidget userId={user._id} user={user} />
       </Box>
-    </InfiniteScrollBase>
+      <Box
+        flexBasis={isNonMobileScreens ? "42%" : undefined}
+        mt={isNonMobileScreens ? undefined : "2rem"}
+      >
+        <PostCreator />
+        <Box m="2rem 0" />
+        <PostsWidget
+          loading={isLoading}
+          posts={posts}
+          totalPages={totalPages}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        />
+      </Box>
+      {isNonMobileScreens && (
+        <Box flexBasis="26%">
+          <FriendListWidget friends={user.friends} userId={user._id} />
+          <Box m="2rem 0" />
+          <SearchFriendWidget userId={user._id} />
+        </Box>
+      )}
+    </Box>
   );
 };
 
--- client/src/pages/ProfilePage/index.jsx
+++ client/src/pages/ProfilePage/index.jsx
@@ -1,5 +1,5 @@
-import { Box, useMediaQuery } from "@mui/material";
-import { useSelector } from "react-redux";
+import { Box, CircularProgress, useMediaQuery } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
 import {
@@ -7,57 +7,102 @@ import {
   FriendListWidget,
   PostsWidget,
   UserWidget,
-  InfiniteScrollBase,
 } from "../../components";
 
 import { MEDIA_QUERY_MIN_WIDTH } from "../../constants/global";
 
-import useGetUserPosts from "../../hooks/usePosts/useGetUserPosts";
-import useGetUser from "../../hooks/useUser/useGetUser";
+import { useEffect, useState } from "react";
+import { getUserPosts } from "../../services/postsServices";
+import { getUser } from "../../services/usersServices";
+import { setPosts } from "../../state/slices/postsSlice";
 
 const ProfilePage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [userProfile, setUserProfile] = useState(null);
+
+  const dispatch = useDispatch();
+
+  const { posts } = useSelector((state) => state.posts);
   const { token } = useSelector((state) => state.auth);
+
   const { userId } = useParams();
+
   const isNonMobileScreens = useMediaQuery(MEDIA_QUERY_MIN_WIDTH[1000]);
 
-  const { userPostsData, hasNextPage, fetchNextPage, isLoading } =
-    useGetUserPosts({ userId, token });
+  const fetchPage = async () => {
+    setIsLoading(true);
+    window.scrollTo({ top: 0, behavior: "smooth" });
 
-  const userData = useGetUser({
-    userId,
-    token,
-  });
+    const dataPosts = await getUserPosts({
+      userId,
+      token,
+      page: currentPage,
+    });
 
-  if (!userData) return null;
+    const dataUser = await getUser({ userId, token });
+
+    dispatch(setPosts(dataPosts.data));
+
+    setUserProfile(dataUser.data);
+    setTotalPages(dataPosts.pageInfo.totalPages);
+
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchPage();
+  }, [currentPage, userId]);
 
   return (
-    <InfiniteScrollBase
-      dataLength={userPostsData.length}
-      hasNextPage={hasNextPage}
-      fetchNextPage={fetchNextPage}
-    >
-      <Navbar isNavigate />
-      <Box
-        width="100%"
-        padding="2rem 6%"
-        display={isNonMobileScreens ? "flex" : "block"}
-        gap="2rem"
-        justifyContent="center"
-      >
-        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
-          <UserWidget user={userData} userId={userId} />
-          <Box m="2rem 0" />
-          <FriendListWidget friends={userData.friends} userId={userId} />
-        </Box>
+    <Box>
+      {isLoading && (
         <Box
-          flexBasis={isNonMobileScreens ? "42%" : undefined}
-          mt={isNonMobileScreens ? undefined : "2rem"}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            mt: "4rem",
+            minHeight: "100vh",
+          }}
         >
-          {/* <PostCreator picturePath={userData.picture} /> */}
-          <PostsWidget posts={userPostsData} loading={isLoading} isProfile />
+          <CircularProgress />
         </Box>
-      </Box>
-    </InfiniteScrollBase>
+      )}
+      {!isLoading && userProfile && (
+        <Box
+          width="100%"
+          padding="2rem 6%"
+          display={isNonMobileScreens ? "flex" : "block"}
+          gap="2rem"
+          justifyContent="center"
+        >
+          <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+            <UserWidget user={userProfile} userId={userId} />
+            <Box m="2rem 0" />
+            <FriendListWidget
+              isLoading={isLoading}
+              friends={userProfile?.friends}
+              userId={userId}
+            />
+          </Box>
+          <Box
+            flexBasis={isNonMobileScreens ? "42%" : undefined}
+            mt={isNonMobileScreens ? undefined : "2rem"}
+          >
+            {/* <PostCreator picturePath={userData.picture} /> */}
+            <PostsWidget
+              posts={posts}
+              loading={isLoading}
+              totalPages={totalPages}
+              currentPage={currentPage}
+              setCurrentPage={setCurrentPage}
+            />
+          </Box>
+        </Box>
+      )}
+    </Box>
   );
 };
 
