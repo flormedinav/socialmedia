589dc876028ff48ef7ee771f2fc4f72dbc1ff1b2
flormedinav <flormedinav7@gmail.com>
2024-02-14 17:47:28 -0300

    fixes general

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,25 +4,20 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 import { ToastContainer } from "react-toastify";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 
 import App from "./App.jsx";
 import "./index.css";
 import store from "./state";
-import { BrowserRouter } from "react-router-dom";
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <QueryClientProvider client={queryClient}>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistStore(store)}>
-        <ToastContainer />
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  </QueryClientProvider>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistStore(store)}>
+      <ToastContainer />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
