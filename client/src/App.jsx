51e66e57eb155def2a2828a1f964fddee93b824b
flormedinav <flormedinav7@gmail.com>
2024-02-14 17:46:07 -0300

    md Footer and Navbar added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import { Navigate, Routes, Route, useLocation } from "react-router-dom";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
@@ -13,39 +13,47 @@ import {
   NotFoundPage,
   RegisterPage,
 } from "./pages";
+import { Navbar, Footer } from "./components";
 
 function App() {
   const { mode } = useSelector((state) => state.general);
   const theme = useMemo(() => createTheme(themeSettings(mode), [mode]));
   const isAuth = Boolean(useSelector((state) => state.auth.token));
 
+  const location = useLocation();
+
   return (
-    <div className="app">
-      <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Routes>
-            <Route
-              path="/"
-              element={isAuth ? <Navigate to="/home" /> : <LoginPage />}
-            />
-            <Route
-              path="/register"
-              element={isAuth ? <Navigate to="/home" /> : <RegisterPage />}
-            />
-            <Route
-              path="/home"
-              element={isAuth ? <HomePage /> : <Navigate to="/" />}
-            />
-            <Route
-              path="/profile/:userId"
-              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
-            />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </ThemeProvider>
-      </BrowserRouter>
-    </div>
+    <ThemeProvider theme={theme}>
+      <>
+        <CssBaseline />
+        {location.pathname !== "/" && location.pathname !== "/register" && (
+          <Navbar isNavigate />
+        )}
+        <Routes>
+          <Route
+            path="/"
+            element={isAuth ? <Navigate to="/home" /> : <LoginPage />}
+          />
+          <Route
+            path="/register"
+            element={isAuth ? <Navigate to="/home" /> : <RegisterPage />}
+          />
+          <Route
+            path="/home"
+            element={isAuth ? <HomePage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/profile/:userId"
+            element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+          />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+
+        {location.pathname !== "/" && location.pathname !== "register" && (
+          <Footer />
+        )}
+      </>
+    </ThemeProvider>
   );
 }
 
