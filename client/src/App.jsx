9913bcb70b82d25c8cda967276f4dc1d36cbf931
flormedinav <flormedinav7@gmail.com>
2024-02-14 19:36:52 -0300

    fixes general

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -49,7 +49,7 @@ function App() {
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
 
-        {location.pathname !== "/" && location.pathname !== "register" && (
+        {location.pathname !== "/" && location.pathname !== "/register" && (
           <Footer />
         )}
       </>
